2a2f4fe932227b106e53ccfe41bec3a522470fd3
Elena Zhuvak <elenkazh@gmail.com>
2025-09-11 17:54:34 +0100

    change name of file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,24 @@
 
 import { Header } from './Header/Header.jsx';
 import css from './App.module.css'
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage.jsx';
+import Catalog from '../pages/12/12.jsx';
+import { Suspense } from 'react';
+import CarDetailsPage from '../pages/CarDetailsPage/CarDetailsPage.jsx';
 
 const App = () => {
   return (
     <div className={css.container}>
       <Header />
+
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/catalog' element={<Catalog />} />
+          <Route path='/catalog/:id' element={<CarDetailsPage />}  />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
