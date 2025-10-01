c17f39dae10b31dcfafe1ee3cbea4003123541a0
Elena Zhuvak <elenkazh@gmail.com>
2025-10-01 21:39:38 +0100

    fix: update logo import and improve formatting in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
-import { NavLink } from "react-router-dom"
-import { buildLinkClass } from "../../helpers/buildLinkClass.js"
-import css from './Header.module.css'
-
+import { NavLink } from 'react-router-dom';
+import { buildLinkClass } from '../../helpers/buildLinkClass.js';
+import css from './Header.module.css';
+import logo from '../../assets/logo.png'
 
 export const Header = () => {
   return (
     <header className={css.header}>
-        <NavLink to='/'>
-            <img src='/src/assets/logo.png' alt='Logo' className={css.logo} />
-        </NavLink>
+      <NavLink to="/">
+        <img src={logo} alt="Car Rental Logo" className={css.logo} />
+      </NavLink>
 
-        <nav className={css.nav}>
-            <NavLink className={buildLinkClass} to='/' end >Home</NavLink>
-            <NavLink className={buildLinkClass} to='/catalog' end >Catalog</NavLink>
-        </nav>
+      <nav className={css.nav}>
+        <NavLink className={buildLinkClass} to="/" end>
+          Home
+        </NavLink>
+        <NavLink className={buildLinkClass} to="/catalog" end>
+          Catalog
+        </NavLink>
+      </nav>
     </header>
-  )
-}
+  );
+};
